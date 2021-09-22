e0492043f2b88365459dbef7c3239d3b005d6582
root <root@localhost.localdomain>
2021-09-21 22:31:03 -0400

    Add Spring Boot skeleton

---
+++ src/main/java/com/automation/CI_CD_Training/CiCdTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.automation.CI_CD_Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automation/CI_CD_Training/CiCdTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.CI_CD_Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
